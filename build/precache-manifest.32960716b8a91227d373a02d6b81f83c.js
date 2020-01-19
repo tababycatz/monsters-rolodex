ed56707a613c8a0e3c71114a5a9570f4a4110d67
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2020-01-18 19:40:12 -0500

    built and updating repo

---
+++ build/precache-manifest.32960716b8a91227d373a02d6b81f83c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56840bd29b72881d5f6f25cee69ec339",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4699054cfe2975f4674a",
+    "url": "/monsters-rolodex/static/css/main.8ad9861f.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4699054cfe2975f4674a",
+    "url": "/monsters-rolodex/static/js/main.7778ee49.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
